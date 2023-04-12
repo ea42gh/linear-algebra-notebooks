353d215d920765b193ff631cd6ccb2e92d55d736
ea42gh <ea42_github@mail.com>
2023-04-12 18:14:39 -0400

    updating Notes

--- Notes/LinAlg/Templates/templatesJS/jupyterlink.js
+++
@@ -1,8 +0,0 @@
-async function jupyterlink( nm, port) {
-  // create a link for the notebook.ipynb  `nm`  for the server running on `port`
-  return "[" + nm + "](" +
-          "http://localhost:"+String(port) + "/lab/workspaces/auto-K/tree/NOTEBOOKS/file:///home/lab/NOTEBOOKS/elementary-linear-algebra/notebooks/" + nm +
-          ")"
-}
-
-module.exports = jupyterlink;
