2050f190fe3a78237f28a88b3451729363089630
ea42gh <ea42_github@mail.com>
2026-01-22 19:17:29 -0500

    pycall crash

--- binder/sitecustomize.py
+++ binder/sitecustomize.py
@@ -6,8 +6,9 @@ os.environ.setdefault("JULIACALL_PROJECT", "/home/jovyan/.julia_env")
 os.environ.setdefault("PYJULIAPKG_PROJECT", "/home/jovyan/.julia_env")
 os.environ.setdefault("JULIAPKG_PROJECT", "/home/jovyan/.julia_env")
 
-try:
-    from juliacall import Main as jl
-    jl.seval('import Pkg; Pkg.activate("/home/jovyan/.julia_env")')
-except Exception:
-    pass
+if os.environ.get("ENABLE_JULIACALL_AUTACTIVATE") == "1":
+    try:
+        from juliacall import Main as jl
+        jl.seval('import Pkg; Pkg.activate("/home/jovyan/.julia_env")')
+    except Exception:
+        pass
